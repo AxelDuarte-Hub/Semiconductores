8d474aa06f46abb307119a23e71008fb2bdf588e
AxelDuarte-Hub <a01646418@tec.mx>
2026-06-13 11:26:00 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,77 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
+// Detector de secuencia 11010 en Verilog (RTL)
+// Integrantes: José Abdiel Lopez y Axel Duarte
+//module sequence_detector_11010 (
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input clk,
+    input rst,
+    input sequence_in,
+    output reg match_sequence
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+// Codificación de estados
+parameter S0 = 3'b000,
+          S1 = 3'b001,
+          S2 = 3'b010,
+          S3 = 3'b011,
+          S4 = 3'b100,
+          S5 = 3'b101;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+reg [2:0] current_state, next_state;
 
+// Registro de estado
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        current_state <= S0;
+    else
+        current_state <= next_state;
+end
+
+// Lógica de transición
+always @(*)
+begin
+    case(current_state)
+        S0:
+            if(sequence_in)
+                next_state = S1;
+            else
+                next_state = S0;
+        S1:
+            if(sequence_in)
+                next_state = S2;
+            else
+                next_state = S0;
+        S2:
+            if(sequence_in)
+                next_state = S2;
+            else
+                next_state = S3;
+        S3:
+            if(sequence_in)
+                next_state = S4;
+            else
+                next_state = S0;
+        S4:
+            if(sequence_in)
+                next_state = S2;
+            else
+                next_state = S5;
+        S5:
+            if(sequence_in)
+                next_state = S4;
+            else
+                next_state = S0;
+        default:
+            next_state = S0;
+    endcase
+end
+
+// Lógica de salida (Moore)
+always @(*)
+begin
+    case(current_state)
+        S5: match_sequence = 1'b1;
+        default: match_sequence = 1'b0;
+    endcase
+end
 endmodule
